a8a1dc20019897ffcd5e436715215244c6fb3e63
soneuik <31829350+soneuik@users.noreply.github.com>
2022-10-10 22:48:54 -0700

    poi

---
+++ lib/models/POI.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:http/http.dart' as http;
+
+//Reference:
+//foursquare that is API used by samsung, uber, and airbnb
+//https://developer.foursquare.com/reference/place-search
+//https://developer.foursquare.com/reference/places-nearby
+class POI {
+  // String temp_max;
+  // String temp_min;
+  // String humidity;
+  // String weather;
+  String apiKey = '5baf4d3d75d0493865be21ec71f9ad59';
+  POI();
+  // Weather(this.temp_max, this.temp_min, this.humidity, this.weather);
+
+  // factory Weather.fromMap(Map<String, dynamic> json) {
+  //   return Weather(
+  //       json['temp_max'], json['temp_min'], json['humidity'], json['weather']);
+  // }
+  // factory Weather.fromJson(Map<String, dynamic> json) {
+  //   return Weather(
+  //       json['temp_max'], json['temp_min'], json['humidity'], json['weather']);
+  // }
+
+//list
+  // curl --request GET \
+  //    --url 'https://api.foursquare.com/v3/places/search?ll=34.052239%2C-118.243398&sort=POPULARITY' \
+  //    --header 'Authorization: fsq33Lsqz7rG2M8eRkSMm8kFXFQKo6EZ6ecw4ei5/ZD2Xf0=' \
+  //    --header 'accept: application/json'
+//get place pics
+// curl --request GET \
+//      --url https://api.foursquare.com/v3/places/4dbf1c816e810768bf9cee43/photos \
+//      --header 'Authorization: fsq33Lsqz7rG2M8eRkSMm8kFXFQKo6EZ6ecw4ei5/ZD2Xf0=' \
+//      --header 'accept: application/json'
+  Future<dynamic> getCityPOI(String s) async {
+    try {
+      //Latitude: 34.052239 ; Longitude: -118.243398 of la
+      var lan = '34.052239';
+      var long = '-118.243398';
+      var cityName = 'Los Angeles';
+      final uri = Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?q=$cityName&units=metric&appid=$apiKey'); // where we are going to connect: API address.
+
+      //write coding here that is working.
+      http.Response response = await http.get(uri);
+      if (response.statusCode == 200) {
+        //Something else to work.
+        return jsonDecode(response.body);
+        //Weather report = Weather.fromJson(response.body);
+        //return report;
+      } else {
+        throw 'Somthing went wrong';
+      }
+    } catch (error) {
+      throw error.toString();
+    }
+  }
+}
---
+++ lib/screens/list_city_poi.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:date_format/date_format.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:tourist_guide_california/models/Weather.dart';
+
+import '../models/POI.dart';
+import 'city_screen.dart';
+
+class CityWeatherScreen extends StatefulWidget {
+  CityWeatherScreen({this.weatherData, this.cityPOIDATA});
+  final weatherData;
+  final cityPOIDATA;
+
+  @override
+  _CityWeatherScreenState createState() => _CityWeatherScreenState();
+}
+
+class _CityWeatherScreenState extends State<CityWeatherScreen> {
+  late int temprature;
+  late String cityName;
+  late String country;
+  late String main;
+  late double speed;
+  late int humidity;
+  late int pressure;
+  var formattedDate;
+
+  Weather weather = Weather();
+  POI poi = POI();
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI(widget.weatherData, widget.cityPOIDATA);
+  }
+
+  void updateUI(dynamic weather, dynamic city_poi) {
+    setState(() {
+      temprature = weather['main']['temp'].toInt();
+      humidity = weather['main']['humidity'].toInt();
+      pressure = weather['main']['pressure'].toInt();
+      speed = weather['wind']['speed'].toDouble();
+      var condition = weather['weather'][0]['id'];
+      main = weather['weather'][0]['description'];
+      cityName = weather['name'];
+      country = weather['sys']['country'];
+      var timezone = weather['timezone'];
+      var currDate = DateTime.now().toUtc().add(Duration(seconds: timezone));
+      formattedDate = formatDate(
+        currDate,
+        [DD, ' | ', M, ' ', dd, ' | ', HH, ':', nn],
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () async {
+                        var currWeather =
+                            await weather.getCurrentLocationData();
+                        var currCityPOI = await poi.getCityPOI("los angeles");
+                        updateUI(currWeather, currCityPOI);
+                      },
+                      child: Icon(
+                        FlutterIcons.location_evi,
+                        size: 50.0,
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () async {
+                        var cityName = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return CityScreen();
+                            },
+                          ),
+                        );
+                        if (cityName != null) {
+                          var currWeatherData =
+                              await weather.getCityWeather("Burbank");
+                          var currCityPOI = await poi.getCityPOI("los angeles");
+                          print(currWeatherData);
+                          updateUI(currWeatherData, currCityPOI);
+                        }
+                      },
+                      child: Icon(
+                        FlutterIcons.search_evi,
+                        size: 50.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '$temprature°',
+                        ),
+                      ],
+                    ),
+                    Text(
+                      main,
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '$cityName, $country',
+                      ),
+                      Text(
+                        formattedDate,
+                      ),
+                      Divider(
+                        color: Colors.white,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Icon(
+                              FlutterIcons.wind_fea,
+                              size: 40.0,
+                            ),
+                            Icon(
+                              FlutterIcons.md_speedometer_ion,
+                              size: 40.0,
+                            ),
+                            Icon(
+                              FlutterIcons.wi_humidity_wea,
+                              size: 40.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(
+                              '${speed.toString()} m/s',
+                            ),
+                            Text(
+                              '${pressure.toString()} Pa',
+                            ),
+                            Text(
+                              '${humidity.toString()} g/m³',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
